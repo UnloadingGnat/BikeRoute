1cb3c24a2211440bb0940c678579ecafbab081e1
Ahmed Mohamed <ahmedmohamed@Ahmeds-MacBook-Pro.local>
2022-11-05 15:12:48 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { useState } from "react";
 
-function App() {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap() {
+  const [userLocation, setuserLocation] = useState({
+    lat: 10.99835602,
+    lng: 77.01502627,
+  });
+  const defaultProps = {
+    center: userLocation,
+    zoom: 11,
+  };
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) =>
+        console.log(
+          `Lat: ${position.coords.latitude} Lng: ${position.coords.longitude}`
+        ),
+      (err) => alert(`Error (${err.code}): ${err.message}`)
+    );
+  } else {
+    alert("Geolocation is not supported by your browser.");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyANEFi4Q4JlBGKxU3Il9MpEph9yM3BZf9c" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
     </div>
   );
 }
-
-export default App;
